2f5d4ccb74cb67f2caedf5a563dbec329ba81b67
Carlos Asparrin Martin <150694554+SujetoDelta-XXI@users.noreply.github.com>
2026-05-21 17:45:22 -0500

    .

---
+++ Semana07-Capas/Negocio/NCategory.cs
@@ -0,0 +1,32 @@
+﻿using Datos;
+using Entidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class NCategory
+    {
+        DCategory datos = new DCategory();
+        public List<Category> Get()
+        {
+            var categories = datos.Get();
+
+            return categories;
+        }
+        public Category GetById(int id)
+        {
+            var category = datos.Get().Where(x => x.IdCategory == id).FirstOrDefault();
+            return category;
+        }
+
+        public List<Category> GetByName(string name)
+        {
+            var categories = datos.Get().Where(x => x.Name.Contains(name)).ToList();
+            return null;
+        }
+    }
+}
---
+++ Semana07-Capas/Semana07-Capas/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Semana07_Capas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
